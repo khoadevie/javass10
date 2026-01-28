3a85df0e1ea342ee8cfe99a48964028837266af2
XLM <khoado.devie@gmail.com>
2026-01-29 03:47:56 +0700

    Add files via upload

---
+++ b1.java
@@ -0,0 +1,20 @@
+class Animal {
+    protected String name;
+
+    public void makeSound() {
+        System.out.println("Some animal sound");
+    }
+}
+
+class Dog extends Animal {
+    public Dog(String name) {
+        this.name = name;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog("Buddy");
+        d.makeSound();
+    }
+}
