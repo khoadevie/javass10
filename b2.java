d33fa6a36d22a08a32dd77374a6a327f47fcfae8
XLM <khoado.devie@gmail.com>
2026-01-29 03:49:39 +0700

    Add files via upload

---
+++ b2.java
@@ -0,0 +1,43 @@
+class Shape {
+    public double area() {
+        return 0;
+    }
+}
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape s1 = new Rectangle(2, 3);
+        Shape s2 = new Circle(1);
+
+        System.out.println("Area Rectangle: " + s1.area());
+        System.out.println("Area Circle: " + s2.area());
+    }
+}
